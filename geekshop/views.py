0194d0f136f5f817c05a3f114cd844bfba67d5e5
timonen87 <71833451+timonen87@users.noreply.github.com>
2021-06-01 14:44:01 +0300

    Добавил популярные товары через контекст

--- geekshop/views.py
+++ geekshop/views.py
@@ -3,10 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     title = 'Интернет магазин'
-    list_params = ['стул', 'стол', 'купить']
+
+
+    tab_content = [
+        {'href': '/img/product-1.jpg', 'name': 'Отличный стул'},
+        {'href': '/img/product-3.jpg', 'name': 'Отличный чайник'},
+        {'href': '/img/product-3-sm.jpg', 'name': 'Отличный диван'},
+        {'href': '/img/product-2.jpg', 'name': 'Стул повышенного качества'},
+    ]
 
     context = {
-        'list_params': list_params,
+        'tab_content': tab_content,
         'some_name': 'hello world',
         'title': title,
     }
