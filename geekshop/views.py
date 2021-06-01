98a94d46f1c8368ae574b422dc74a257930b22c6
timonen87 <71833451+timonen87@users.noreply.github.com>
2021-06-01 15:05:34 +0300

    Добавил похожие продукты через контекст

--- geekshop/views.py
+++ geekshop/views.py
@@ -6,15 +6,14 @@ def index(request):
 
 
     tab_content = [
-        {'href': '/img/product-1.jpg', 'name': 'Отличный стул'},
-        {'href': '/img/product-3.jpg', 'name': 'Отличный чайник'},
-        {'href': '/img/product-3-sm.jpg', 'name': 'Отличный диван'},
-        {'href': '/img/product-2.jpg', 'name': 'Стул повышенного качества'},
+        {'href': '/img/product-1.jpg', 'name': 'Отличный стул', 'icon_hover': '/img/icon-hover.png'},
+        {'href': '/img/product-3.jpg', 'name': 'Отличный чайник','icon_hover': '/img/icon-hover.png'},
+        {'href': '/img/product-3-sm.jpg', 'name': 'Отличный диван', 'icon_hover': '/img/icon-hover.png'},
+        {'href': '/img/product-2.jpg', 'name': 'Стул повышенного качества', 'icon_hover': '/img/icon-hover.png'},
     ]
 
     context = {
         'tab_content': tab_content,
-        'some_name': 'hello world',
         'title': title,
     }
     return render(request, 'index.html', context=context)
--- mainapp/views.py
+++ mainapp/views.py
@@ -12,8 +12,15 @@ def products(request):
         {'href': 'mainapp:index', 'name': 'классика'},
     ]
 
+    related_products = [
+        {'href': '/mainapp/img/product-11.jpg', 'name': 'Стул повышенного качества', 'icon_hover':'/mainapp/img/icon-hover.png'},
+        {'href': '/mainapp/img/product-21.jpg', 'name': 'Стул повышенного качества', 'icon_hover':'/mainapp/img/icon-hover.png'},
+        {'href': '/mainapp/img/product-31.jpg', 'name': 'Стул повышенного качества','icon_hover':'/mainapp/img/icon-hover.png'},
+    ]
+
     context = {
         'links_menu': links_menu,
         'title': title,
+        'related_products':related_products,
     }
     return render(request, 'mainapp/products.html', context=context)
