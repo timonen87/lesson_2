e2c528dafb24eb6aad4dd06608b09e724d20ec3b
timonen87 <71833451+timonen87@users.noreply.github.com>
2021-06-01 14:04:34 +0300

    update lesson2

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nrl2syr@4&@%-v$x6sq$(p($gj97rq!md6z2!_b96b36kljnsq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
