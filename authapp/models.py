1d937efce7180c77f8e7fbb76173d8a7e45e4831
Artem <timonen.as@gmail.com>
2021-06-15 12:45:46 +0300

    Update lesson 6

--- authapp/models.py
+++ authapp/models.py
@@ -5,3 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatars', blank=True)
     age = models.PositiveIntegerField(verbose_name='возраст')
+    
