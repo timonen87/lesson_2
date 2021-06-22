39edf5517ace501cf5ffe6d5807960ca990672ab
timonen87 <71833451+timonen87@users.noreply.github.com>
2021-06-22 11:02:31 +0300

    Update Lesson 7

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'mainapp',
     'authapp',
     'basketapp',
+    'adminapp',
 ]
 
 AUTH_USER_MODEL = 'authapp.ShopUser'
